cf02eef14988d899311baad9e7133afd4669cedd
andy <66240551+flandolf@users.noreply.github.com>
2023-01-30 23:34:46 +1100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'adrod13 installer',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         useMaterial3: false,
